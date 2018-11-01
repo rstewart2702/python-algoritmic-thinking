7d67abd87f1ac6d120fa93b2cfc981df68f399ef
Richard S. Stewart <rstewart2702@gmail.com>
2018-11-01 11:16:00 -0500

    Major revision to the "distance" function: Given nodes node1 and node2, the root-to-node paths contain a common prefix, which terminates at a "deepest common ancestor."  This algorithm keeps track of that deepest common ancestor, so that "ascencion from node1" returns to that ancestor node, and then the "descent to node2" may start from there.  The algorithm counts the number of edges traversed up to the "deepest common ancestor" and then the number of edges traversed down to node2 from that "deepest common ancestor."
    N.B. It's possible for that deepest common ancestor to be either of node1 or node2.

--- bstins.py
+++ bstins.py
@@ -51,9 +51,8 @@ def search(t,ptr,v):
         return search(t,t[ptr]["r"],v)
     else:
         return search(t,t[ptr]["l"],v)
-    
 
-def distShell(t,node1, node2):
+def distShell(t,node1,node2):
     # swap the ordering of node1 and node2, just to guarantee
     # that node1 < node2:
     if (node2 < node1):
@@ -61,41 +60,53 @@ def distShell(t,node1, node2):
         node2=node1
         node1=tmp
     #
-    curr = 0
-    stk = []
-    hop = 0
-    while (t[curr]["key"] != node1):
+    # Traverse down to node1, looking for the "lowest-in-the-tree" common
+    # vertex on both paths from node1 to node2.
+    # This common-vertex will be the place to which the algorithm
+    # ascends after finding node1, in order to compute the distance from node1 to node2.
+    curr, stk, hop, dca = 0, [], 0, 0
+    while (    not(node1 < t[curr]['key'] and t[curr]['key'] < node2)
+           and node1 != t[curr]['key']
+           and node2 != t[curr]['key']):
         stk.append(curr)
-        if (t[curr]['key'] <= node1):
-            curr = t[curr]["r"]
+        if (t[curr]['key'] < node1):
+            curr = t[curr]['r']
         else:
-            curr = t[curr]["l"]
+            curr = t[curr]['l']
     #
-    print ("found ",node1,", now searching for ",node2,":")
-    if (curr != 0):
-        parent = stk[len(stk)-1]
-    else:
-        parent = -1
-    while (    curr != 0
-           and not(t[curr]['key'] < node2 and node2 < t[parent]['key'])):
-        print ("ascending; ", node2, " does NOT fall between ", t[curr]['key'], "and ", t[parent]['key'])
-        hop = hop + 1
+    # so now:
+    #     t[curr]['key'] < node1 and t[curr]['key'] < node2
+    #  or node1 == t[curr]['key']
+    #  or node2 == t[curr]['key']
+    # and so we should store curr into dca, no?
+    dca = curr
+    print ("Deepest common ancestor is", t[dca]['key'])
+    #
+    # Now we must continue the search down to node1:
+    while(t[curr]['key'] != node1):
+        stk.append(curr)
+        if (t[curr]['key'] < node1):
+            curr = t[curr]['r']
+        else:
+            curr = t[curr]['l']
+    #
+    # Now t[curr]['key'] == node1.
+    # Now we traverse back up to the "deepest-common-ancestor," dca,
+    # from which we may need to traverse "downwards" in order to search for node2:
+    while(curr != dca):
         curr = stk.pop()
-        if (curr != 0):
-            parent = stk[len(stk)-1]
-        else:
-            parent = -1
+        hop = hop + 1
     #
+    # Now curr == dca.
     while (t[curr]['key'] != node2):
         hop = hop + 1
         if (t[curr]['key'] < node2):
-            print ("descending to the right of ", t[curr]['key'])
             curr = t[curr]['r']
         else:
-            print ("descending to the left of ", t[curr]['key'])
             curr = t[curr]['l']
     #
     return hop
+
 
 def buildTree(values):
     r = {"key": values[0], "l" : -1, "r": -1 }
