fdcfd0b5c57438c749606e89788e5e1fd8a8bbda
Richard S. Stewart <rstewart2702@gmail.com>
2018-11-02 17:31:26 -0500

    Further improvement:  distance can be calculated by counting number of edges traversed down to each of node1 and node2 from the so-called deepest-common-ancestor vertex...

--- bstins.py
+++ bstins.py
@@ -23,6 +23,16 @@ def search(t,ptr,v):
     else:
         return search(t,t[ptr]["l"],v)
 
+def downTo(t,curr,node):
+    hop = 0
+    while (t[curr]['key'] != node):
+        if (t[curr]['key'] < node):
+            hop, curr = hop+1, t[curr]['r']
+        else:
+            hop, curr = hop+1, t[curr]['l']
+    return hop
+        
+
 def distShell(t,node1,node2):
     # swap the ordering of node1 and node2, just to guarantee
     # that node1 < node2:
@@ -35,49 +45,32 @@ def distShell(t,node1,node2):
     # vertex on both paths from node1 to node2.
     # This common-vertex will be the place to which the algorithm
     # ascends after finding node1, in order to compute the distance from node1 to node2.
-    curr, stk, hop, dca = 0, [], 0, 0
-    while (    not(node1 < t[curr]['key'] and t[curr]['key'] < node2)
-           and node1 != t[curr]['key']
-           and node2 != t[curr]['key']):
-        stk.append(curr)
+    curr = 0
+    # N.B. node1 < node2,
+    # AND
+    # the expression:
+    #       t[curr]['key'] != node1 and t[curr]['key'] != node2
+    #   and not(node1 < t[curr]['key'] and t[curr]['key'] < node2)
+    # simplifies to:
+    #   t[curr]['key'] < node1 or node2 < t[curr]['key']
+    # which is the loop guard used here.
+    while (t[curr]['key'] < node1 or node2 < t[curr]['key']):
+        # i.e., while both node1 and node2 lie to the right of the current node
+        #          or both node1 and node2 lie to the left of the current node
+        # then we haven't found the "deepest common ancestor" yet.
         if (t[curr]['key'] < node1):
             curr = t[curr]['r']
         else:
             curr = t[curr]['l']
     #
     # so now:
-    #     t[curr]['key'] < node1 and t[curr]['key'] < node2
+    #     node1 < t[curr]['key'] and t[curr]['key'] < node2
     #  or node1 == t[curr]['key']
     #  or node2 == t[curr]['key']
     # and so we should store curr into dca, no?
-    dca = curr
-    print ("Deepest common ancestor is", t[dca]['key'])
+    print ("Deepest common ancestor is", t[curr]['key'])
     #
-    # Now we must continue the search down to node1:
-    while(t[curr]['key'] != node1):
-        stk.append(curr)
-        if (t[curr]['key'] < node1):
-            curr = t[curr]['r']
-        else:
-            curr = t[curr]['l']
-    #
-    # Now t[curr]['key'] == node1.
-    # Now we traverse back up to the "deepest-common-ancestor," dca,
-    # from which we may need to traverse "downwards" in order to search for node2:
-    while(curr != dca):
-        curr = stk.pop()
-        hop = hop + 1
-    #
-    # Now curr == dca.
-    while (t[curr]['key'] != node2):
-        hop = hop + 1
-        if (t[curr]['key'] < node2):
-            curr = t[curr]['r']
-        else:
-            curr = t[curr]['l']
-    #
-    return hop
-
+    return downTo(t,curr,node1) + downTo(t,curr,node2)
 
 def buildTree(values):
     r = {"key": values[0], "l" : -1, "r": -1 }
@@ -96,4 +89,19 @@ def bstDistance(values, n, node1, node2):
     # 
     #
     return distShell(tree,node1,node2)
+
+if __name__ == "__main__":
+    print bstDistance([10,9,8,5,6,7,3,4,2,1],10,7,8) == 3
+    print bstDistance([10,9,8,5,6,7,3,4,2,1],10,1,7) == 5
+    print bstDistance([1,8,9,10,5,6,7,3,4,2],10,7,8) == 3
+    print bstDistance([1,8,9,10,5,6,7,3,4,2],10,4,7) == 4
+    #
+    print bstDistance([8,48,32,46,2,1,5,3],8,2,3) == 2
+    print bstDistance([8,48,32,46,2,1,5,3],8,3,48) == 4
+    #
+    print bstDistance([8,2,48,1,5,32,46,3],7,3,1) == 3
+    print bstDistance([8,2,48,1,5,32,46,3],7,32,48) == 1
+    print bstDistance([8,2,48,1,5,32,46,3],7,32,8) == 2
+    print bstDistance([8,2,48,1,5,32,46,3],7,3,32) == 5
+    
 
