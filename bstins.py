0792cffbe064246ca257bb07afcf01afa6f3d4e7
Richard S. Stewart <rstewart2702@gmail.com>
2019-04-05 15:17:11 -0500

    Changed to a more OO-style.

--- bstins.py
+++ bstins.py
@@ -6,28 +6,77 @@
 class BSTree:
     def __init__(self, node):
         self._root = node
-    #
-    def _insert(self, node, v):
-        if (node.key() < v):
-            if (node.rChild == None):
-                node
 
     def insert(self, v):
-        if (self._root = None):
-            return self._root
-        self._insert(self._root, v)
+        if self._root is None:
+            self._root = BSTNode(v, None, None)
+        else:
+            self._insert(self._root, v)
 
     def _insert(self, t, v):
         if (t.key() < v):
-            if (t.rChild() = None):
+            if (t.rChild() is None):
                 t._rChild = BSTNode(v, None, None)
             else:
                 self._insert(t.rChild(),v)
         else:
-            if(t.lChild() = None):
+            if(t.lChild() is None):
                 t._lChild = BSTNode(v, None, None)
             else:
                 self._insert(t.lChild(),v)
+    #
+    def buildTree(self, values):
+        for v in values:
+            self.insert(v)
+    #
+    def inorderPrint(self):
+        if self._root is None:
+            return
+        self._inorderPrint(self._root)
+        
+    def _inorderPrint(self, treeRoot):
+        if treeRoot is None:
+            return  # i.e., there's nothing to print!
+        #
+        self._inorderPrint(treeRoot.lChild())
+        print ("value: ", treeRoot.key())
+        self._inorderPrint(treeRoot.rChild())
+
+    def bstDistance(self, v1, v2):
+        if v2 < v1:
+            v1, v2 = v2, v1  # swap them!
+        #
+        treeRoot = self._root
+        #
+        if treeRoot is None:
+            return 0  # what is the distance when the tree is "empty?"
+        #
+        distance = 0
+        #
+        while v2 < treeRoot.key() or treeRoot.key() < v1:
+            if v2 < treeRoot.key():
+                treeRoot = treeRoot.lChild()
+            else:
+                treeRoot = treeRoot.rChild()
+        #
+        # Now, treeRoot.key() <= v2 and v1 <= treeRoot.key()
+        # Assuming that v1 != v2, then:
+        # we must calculate the path length from treeRoot down to v1
+        # and then calculate the path length from treeRoot down to v2:
+        return self._pathFromRoot(treeRoot, v1) + self._pathFromRoot(treeRoot, v2)
+
+    def _pathFromRoot(self, treeRoot, v):
+        dist = 0
+        #
+        while treeRoot is not None and treeRoot.key() != v:
+            if treeRoot.key() < v:
+                treeRoot = treeRoot.rChild()
+            else:
+                treeRoot = treeRoot.lChild()
+            #
+            dist = dist + 1
+        #
+        return dist
 
 class BSTNode:
     def __init__(self, data, lChild, rChild):
@@ -42,114 +91,24 @@ class BSTNode:
         return self._rChild
 
     def key(self):
-        return self._data._key
+        return self._data
 
-    
-
-    
-def insert(t,ptr,v):
-    if (t[ptr]['key'] < v):
-        if (t[ptr]['r'] == -1):
-            t.append({'key': v, 'l': -1, 'r': -1})
-            t[ptr]['r'] = len(t)-1
-        else:
-            insert(t,t[ptr]['r'],v)
-    else:
-        if (t[ptr]['l'] == -1):
-            t.append({'key': v, 'l': -1, 'r': -1})
-            t[ptr]['l'] = len(t)-1
-        else:
-            insert(t,t[ptr]['l'],v)
-
-def search(t,ptr,v):
-    if (ptr == -1):
-        return False
-    if (t[ptr]["key"] == v):
-        return True
-    if (t[ptr]["key"] < v):
-        return search(t,t[ptr]["r"],v)
-    else:
-        return search(t,t[ptr]["l"],v)
-
-def downTo(t,curr,node):
-    hop = 0
-    while (t[curr]['key'] != node):
-        if (t[curr]['key'] < node):
-            hop, curr = hop+1, t[curr]['r']
-        else:
-            hop, curr = hop+1, t[curr]['l']
-    return hop
-        
-
-def distShell(t,node1,node2):
-    # swap the ordering of node1 and node2, just to guarantee
-    # that node1 < node2:
-    if (node2 < node1):
-        tmp=node2
-        node2=node1
-        node1=tmp
-    #
-    # Traverse down to node1, looking for the "lowest-in-the-tree" common
-    # vertex on both paths from node1 to node2.
-    # This common-vertex will be the place to which the algorithm
-    # ascends after finding node1, in order to compute the distance from node1 to node2.
-    curr = 0
-    # N.B. node1 < node2,
-    # AND
-    # the expression:
-    #       t[curr]['key'] != node1 and t[curr]['key'] != node2
-    #   and not(node1 < t[curr]['key'] and t[curr]['key'] < node2)
-    # simplifies to:
-    #   node2 < t[curr]['key'] or t[curr]['key'] < node1
-    # which is the loop guard used here.
-    while (t[curr]['key'] < node1 or node2 < t[curr]['key']):
-        # i.e., while both node1 and node2 lie to the right of the current node
-        #          or both node1 and node2 lie to the left of the current node
-        # then we haven't found the "deepest common ancestor" yet.
-        if (t[curr]['key'] < node1):
-            curr = t[curr]['r']
-        else:
-            curr = t[curr]['l']
-    #
-    # so now:
-    #     node1 < t[curr]['key'] and t[curr]['key'] < node2
-    #  or node1 == t[curr]['key']
-    #  or node2 == t[curr]['key']
-    # and so we should store curr into dca, no?
-    print ("Deepest common ancestor is", t[curr]['key'])
-    #
-    return downTo(t,curr,node1) + downTo(t,curr,node2)
-
-def buildTree(values):
-    r = {"key": values[0], "l" : -1, "r": -1 }
-    tree = [r]
-    #
-    for v in values[1:]:
-        insert(tree,0,v)
-    return tree
-
-def bstDistance(values, n, node1, node2): 
-    # create root node:
-    if (n == 0):
-        return -1
-    #
-    tree = buildTree(values)
-    # 
-    #
-    return distShell(tree,node1,node2)
 
 if __name__ == "__main__":
-    print bstDistance([10,9,8,5,6,7,3,4,2,1],10,7,8) == 3
-    print bstDistance([10,9,8,5,6,7,3,4,2,1],10,1,7) == 5
-    print bstDistance([1,8,9,10,5,6,7,3,4,2],10,7,8) == 3
-    print bstDistance([1,8,9,10,5,6,7,3,4,2],10,4,7) == 4
+    myT = BSTree(BSTNode(1,None,None))
     #
-    print bstDistance([8,48,32,46,2,1,5,3],8,2,3) == 2
-    print bstDistance([8,48,32,46,2,1,5,3],8,3,48) == 4
+    myT.insert(9)
     #
-    print bstDistance([8,2,48,1,5,32,46,3],7,3,1) == 3
-    print bstDistance([8,2,48,1,5,32,46,3],7,32,48) == 1
-    print bstDistance([8,2,48,1,5,32,46,3],7,32,8) == 2
-    print bstDistance([8,2,48,1,5,32,46,3],7,3,32) == 5
-    
+    myT.inorderPrint()
+    #
+    print (myT.bstDistance(1, 9))
+    #
+    myT2 = BSTree(None)
+    myT2.buildTree([10,9,8,5,6,7,3,4,2,1])
+    myT2.inorderPrint()
+    print (myT2.bstDistance(7,8) == 3)
 
+    myT3=BSTree(None)
+    myT3.buildTree([1,8,9,10,5,6,7,3,4,2])
+    print (myT3.bstDistance(4,7) == 4)
+    
