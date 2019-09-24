11b1389800fc9a2c8ea9be78b1f14cd26ccc1a22
Richard S. Stewart <rstewart2702@gmail.com>
2019-09-24 13:24:38 -0500

    A first foray into building a union-find/"disjoint-sets" implementation.

---
+++ DisjointSets.py
@@ -0,0 +1,20 @@
+# union-find, disjoin-set implementation?
+from graph import Vertex as Vertex, Graph as Graph, GraphPaths as GraphPaths
+
+class UFNode:
+    def __init__(self, vtx):
+        self.vtx = vtx
+        self.parent = None
+    #
+
+class DisjointSets:
+    def __init__(self, vertices):
+        self.sets = {}
+        for v in vertices:
+            self.sets[v.vtx] = UFNode(v)
+    #
+    def union(self, v1, v2):
+        self.sets[v2].parent = v1
+    #
+#     def find(self, vtx):
+        
